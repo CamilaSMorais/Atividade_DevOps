1d440344db04f7109c9a6eb882798b0de3439fc7
Camila Morais <m.silva52@pucpr.edu.br>
2025-03-28 20:42:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # 127.0.0.1:8000
-@app.get("/")
+@app.get("/Olá Mundo")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Olá Mundo"}
 
 # 127.0.0.1:8000/teste1
-@app.get("/teste1")
+@app.get("/funcaoteste")
 async def funcaoteste():
     return {"teste": "deu certo"}
